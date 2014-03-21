bc2b124d4ad38aecce7e153dddfa07b47e3cfc8e
David Pede <david.pede@tasianmedia.com>
2014-03-21 16:05:54 +0000

    Update Licence & Copyright

--- core/components/getdate/elements/snippets/snippet.getdate.php
+++ core/components/getdate/elements/snippets/snippet.getdate.php
@@ -8,21 +8,23 @@
  * @since November 22, 2013
  * @package getdate
  *
- *getDate is free software; you can redistribute it and/or modify it under the
- *terms of the GNU General Public License as published by the Free Software
- *Foundation; either version 3 of the License, or any later version.
-
- *getDate is distributed in the hope that it will be useful, but WITHOUT ANY
- *WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
- *You should have received a copy of the GNU General Public License along with
- *getDate; if not, write to the Free Software Foundation, Inc., 59 Temple
- *Place, Suite 330, Boston, MA 02111-1307 USA
+ * Copyright (C) 2013 David Pede. All rights reserved. <dev@tasianmedia.com>
+ *
+ * getDate is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or any later version.
+ *
+ * getDate is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * getDate; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 /* set default properties */
-$offset = !empty($offset) ? $offset : 'now';
+$offset = !empty($offset) ? $offset : '';
 
 $output = '';
 
